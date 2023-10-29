566e9f64f4cbbeb4889cd0bb08493222d879017a
Mansoor Khan <mansoorKh1098@gmail.com>
2023-10-30 00:06:10 +0530

    🔥 feat: add domin

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDATc-65KdaRxfBWjCsZI1NsMqTIRFuaJM",
+  //   authDomain: "redux-auth-project-6c82b.firebaseapp.com",
+  authDomain: "react-redux-thunk-bay.vercel.app",
+  databaseURL: "https://redux-auth-project-6c82b-default-rtdb.firebaseio.com",
+  projectId: "redux-auth-project-6c82b",
+  storageBucket: "redux-auth-project-6c82b.appspot.com",
+  messagingSenderId: "289650546872",
+  appId: "1:289650546872:web:8941038338f57ad5df7c4d",
+};
+
+export default firebaseConfig;
